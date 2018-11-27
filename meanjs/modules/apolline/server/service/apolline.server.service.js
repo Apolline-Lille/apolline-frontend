d2973d0be3950c03358dba40fede436aa9ac01dc
ABaldeck <arthur.baldeck04@gmail.com>
2018-11-27 16:15:11 +0100

    get the measurements and the data with one measurement but can't reach the value of the JSON

--- meanjs/modules/apolline/server/service/apolline.server.service.js
+++ meanjs/modules/apolline/server/service/apolline.server.service.js
@@ -1,68 +1,165 @@
 'use strict';
-var Influx = require('influx'); 
+
 var jsonexport = require('jsonexport/dist');
 var fs = require('fs');
-var util = require('util');
-
-var apiApolline = 'http://apolline.lille.inria.fr:8086/';
-//'http://apolline.lille.inria.fr:8086/';
-
+var http = require('http');
 /**
- * Get all the data of the DataBase \"campaign\"
+ * Get all the dat
+ * 
+ * 
+ * a of the DataBase \"campaign\"
  * Return the whole database choosen \"campaign\" in a CSV file
  *
  * campaign String name of the database choosen
  * no response value expected for this operation
  **/
 
-//port connexion: http://root:root@127.0.0.1:8086
+//port connexion: http://root:root@localhost:8086/
 //pour localDB: NOAA_water_database
 //measurements: 'average_temperature','h2o_feet','h2o_pH','h2o_quality','h2o_temperature'
 
 var dataTable = new Array();
 var listMeasurements = new Array();
+var dataMeasurement = new Array();
 
+//fonctionne une fois sur 2
 //main function launch when we wrote "curl http://0.0.0.0:80/measurements/{database}"
-exports.measurementsCampaignGET = async (campaign) => {
-  return new Promise(async (resolve,reject) =>{
+
+//requête à passer en node
+//"curl -G 'http://apolline.lille.inria.fr:8086/query?db=loa' --data-urlencode 'q=SELECT * FROM "pm.10.value"'"
+
+
+exports.measurementsCampaignGET = async(campaign) => {
+
     console.log("Lancement de getAllMeasurements");
-    const influx = new Influx.InfluxDB(apiApolline);
-    await influx.getMeasurements(campaign).then( async (measurements) => {
-      return createMeasurementsTable(measurements);
-    }).then (async (listMeasurements) => {
-      const start = async () => {
+    await getMeasurements(campaign).then( async () => {
+      return await listMeasurements;
+    }).then (async () => {
+      console.log(listMeasurements.results.series.values);
+      listMeasurements.values().forEach((measurement) => {
+        console.log(measurement);
+      });
+      /*const start = async () => {
         var i = 0;
-        await asyncForEach(listMeasurements, async (measurement) => {
+        await asyncForEach(listMeasurements.values(), async (measurement) => {
           avancement(i);
+          console.log(listMeasurements.values);
           await getDataFromMeasurement(measurement, campaign);
           i++;
+          await dataTable.push(dataMeasurement);
+          dataMeasurement.splice(0,dataMeasurement.length)
         });
         return dataTable;
       }
-      return start();
-    }).then ( async (table) => {
-      console.log("tu as tous les résultats");
-      console.log(table.length);
-      await jsonexport(table,function(err, csv){
-        if(err) {
-          console.log("nul");
-          return console.log(err);
-        }
-        console.log("csv : \n" + csv );
-        fs.writeFileSync("./CSVDownload/data" + Date.now() + ".csv", console.log(csv), "UTF-8");
-        return csv;
-      });
-    }).then( async (value) => {
-      console.log("ok");
-      console.log(value);
+      return start();*/
+    }).then( () => {
+      console.log(dataTable);
     }).catch( async (err) => {
       console.log("erreur forEach async");
       console.log(err);
     });
-    return resolve();
-  }).catch( async (err) => {
-    console.log("main err");
-    console.log(err);
+}
+
+const getMeasurements = async(campaign) => {
+  var request = encodeURIComponent("SHOW MEASUREMENTS");
+  var options = {
+    host: "apolline.lille.inria.fr",
+    port: 8086,
+    path: "/query?db="+campaign+"&q="+request,
+    method: 'GET'
+  };
+
+  var url = "http://" + options.host + ":" + options.port + options.path;
+  console.log(url);
+
+  http.get(url, (res) => {
+    const { statusCode } = res;
+    const contentType = res.headers['content-type'];
+
+    let error;
+    if (statusCode !== 200) {
+      error = new Error('Request Failed.\n' +
+                        `Status Code: ${statusCode}`);
+    } else if (!/^application\/json/.test(contentType)) {
+      error = new Error('Invalid content-type.\n' +
+                        `Expected application/json but received ${contentType}`);
+    }
+    if (error) {
+      console.error(error.message);
+      // consume response data to free up memory
+      res.resume();
+      return;
+    }
+
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', (chunk) => {
+      rawData += chunk;
+    });
+    res.on('end', () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+        listMeasurements.push(parsedData);
+        console.log(parsedData.results["series"]);
+        console.log(rawData);
+        console.log("listMeasurement "+listMeasurements);
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
+  }).on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
+  });
+}
+
+const getDataFromMeasurement = async(measurement, campaign) => {
+  var request = encodeURIComponent("SELECT * FROM \"" + measurement + "\"");
+  var options = {
+    host: "apolline.lille.inria.fr",
+    port: 8086,
+    path: "/query?db="+campaign+"&q="+request,
+    method: 'GET'
+  };
+
+  var url = "http://" + options.host + ":" + options.port + options.path;
+  console.log(url);
+
+  http.get(url, (res) => {
+    const { statusCode } = res;
+    const contentType = res.headers['content-type'];
+
+    let error;
+    if (statusCode !== 200) {
+      error = new Error('Request Failed.\n' +
+                        `Status Code: ${statusCode}`);
+    } else if (!/^application\/json/.test(contentType)) {
+      error = new Error('Invalid content-type.\n' +
+                        `Expected application/json but received ${contentType}`);
+    }
+    if (error) {
+      console.error(error.message);
+      // consume response data to free up memory
+      res.resume();
+      return;
+    }
+
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', (chunk) => {
+      rawData += chunk;
+    });
+    res.on('end', () => {
+      try {
+        dataMeasurement.push(rawData);
+        const parsedData = JSON.parse(rawData);
+        console.log(dataMeasurement);
+        console.log(parsedData);
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
+  }).on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
   });
 }
 
@@ -71,62 +168,8 @@ const asyncForEach = async (array, callback) =>{
     await callback(array[index], index, array);
   }
 }
+
+
 const avancement = async (i) =>{
   console.log(i);
 }
-
-const getDataFromMeasurement = async (measurement, campaign) => {
-  var request = "select * from \"" + measurement + "\" limit 50";
-  const influxQuery = new Influx.InfluxDB(apiApolline + campaign);
-  return new Promise(async (resolve, reject) => {
-    await influxQuery.query(request).then(async (results) => {
-      dataTable.push(results);
-    }).catch( err => {
-      console.log(err);
-    });
-    return resolve(await dataTable);
-  }).catch( async (err) => {
-    console.log("erreur getDataFromMeasurements");
-    console.log(err);
-    reject();
-  });
-}
-
-
-const createMeasurementsTable = async (measurements) =>{
-  return new Promise(async (resolve, reject) =>{
-    await measurements.forEach(async (measurement) =>{
-      listMeasurements.push(measurement);
-    });
-    console.log("listMeasurements: " + listMeasurements);
-    return resolve(await listMeasurements);
-  }).catch( async (err) => {
-    console.log("create table measurements");
-    console.log(err);
-    return reject();
-  });  
-}
-
-
-//create the CSV file with the JSON DataTable
-var getCSV = async (dataArray) =>{
-  console.log("getCSV");
-  jsonexport(dataArray,async (err, csv) =>{
-    if(err) {
-      console.log("nul");
-      return console.log(err);
-    } 
-    console.log(csv);
-  });
-}
-
-function createFile(data){
-  fs.writeFile('telechargement/formList.csv', data, 'utf8', function (err) {
-    if (err) {
-      console.log('Some error occured - file either not saved or corrupted file saved.');
-    } else{
-      console.log('It\'s saved!');
-    }
-  });
-}
-
